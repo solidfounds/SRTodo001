e0c84da5197ad1f8e6c66c83cd3c2f074db3e4d7
solidfounds <solidfoundsss@gmail.com>
2016-01-11 14:29:52 -0600

    ignodado __pycache__

--- productos/views.py
+++ productos/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Producto
 # Create your views here.
+def producto(request):
+    productos = Producto.objects.all()
+    return render(request, 'producto/product.html', {'producto':productos})
